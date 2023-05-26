a24f2bc5fdc607a3addddef5b5e9548cd45997e1
chandan1393 <chandansharma1393@gmail.com>
2023-05-26 19:00:00 +0530

    api gateway

---
+++ api-gateway/src/main/java/com/chandan/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+/*
+ * package com.chandan.config;
+ * 
+ * import org.springframework.beans.factory.annotation.Value; import
+ * org.springframework.context.annotation.Bean; import
+ * org.springframework.context.annotation.Configuration; import
+ * org.springframework.security.config.annotation.authentication.builders.
+ * AuthenticationManagerBuilder; import
+ * org.springframework.security.config.annotation.web.builders.HttpSecurity;
+ * import org.springframework.security.config.annotation.web.configuration.
+ * EnableWebSecurity; import
+ * org.springframework.security.config.annotation.web.configuration.
+ * WebSecurityConfigurerAdapter; import
+ * org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; import
+ * org.springframework.security.crypto.password.PasswordEncoder;
+ * 
+ * @Configuration
+ * 
+ * @EnableWebSecurity public class SecurityConfig extends
+ * WebSecurityConfigurerAdapter {
+ * 
+ * @Value("${app.username}") String userName;
+ * 
+ * @Value("${app.password}") String password;
+ * 
+ * 
+ * 
+ * @Override protected void configure(AuthenticationManagerBuilder auth) throws
+ * Exception { System.out.println(password); auth.inMemoryAuthentication()
+ * .withUser(userName) .password(passwordEncoder().encode(password))
+ * .roles("user")
+ * 
+ * ; }
+ * 
+ * @Override protected void configure(HttpSecurity httpSecurity) throws
+ * Exception { httpSecurity.csrf().disable() .authorizeRequests().anyRequest()
+ * .authenticated() .and().httpBasic(); }
+ * 
+ * @Bean public PasswordEncoder passwordEncoder() { return new
+ * BCryptPasswordEncoder(); }
+ * 
+ * }
+ */
